ebab4436b3707b10ec1851a7d5ffcefa9caa2bdb
SirCryptic <48811414+SirCryptic@users.noreply.github.com>
2023-02-23 15:44:43 +0000

    now also uses the phonebook.txt option

--- tts-sms.sh
+++ tts-sms.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Developer: SirCryptic (NullSecurityTeam)
-# Info: tts-sms script v1.0 {BETA}
+# Info: sms script v1.0 {BETA}
 # Disclaimer: DO NOT USE THIS TO TROLL OR SPAM PEOPLE I WILL NOT BE RESPONSIBLE FOR YOUR USAGE!
 
 # Details: Get a API key from here: https://www.clicksend.com
 
 # Set the ClickSend API credentials
-api_username="your_username"
-api_key="your_api_key"
+api_username="scns1337"
+api_key="AEDCE6CF-AB1B-08B9-FE99-B3AE0B166B72"
 
 # Set the API endpoint and headers
 url="https://rest.clicksend.com/v3/voice/send"
@@ -17,13 +17,78 @@ headers=(
   "Content-Type: application/json"
 )
 
-# Prompt the user to enter the message
-echo "Enter the message to send:"
-read message_body
+# Set the name of the phone book file
+phone_book_file="phonebook.txt"
 
-# Prompt the user to enter the recipient's phone number
-echo "Enter the recipient's phone number (including country code, e.g. +1234567890):"
-read to_number
+# Define an associative array to store phone numbers and their associated names
+declare -A phone_book
+
+# Load phone numbers from the phone book file
+if [[ -f "$phone_book_file" ]]; then
+  while IFS= read -r line; do
+    number=$(echo "$line" | cut -d' ' -f1)
+    name=$(echo "$line" | cut -d' ' -f2-)
+    phone_book[$number]=$name
+  done < "$phone_book_file"
+fi
+
+# Function to add a phone number to the phone book
+function add_number {
+  echo "Enter the phone number:"
+  read number
+  echo "Enter a name for the recipient:"
+  read name
+  phone_book[$number]=$name
+  echo "Added $name ($number) to the phone book."
+  echo "$number,$name" >> "$phone_book_file"
+}
+
+# Prompt the user to select a country
+echo "Select a country code:"
+select country_code in "US" "AU" "GB"
+do
+  if [[ -n $country_code ]]; then
+    break
+  fi
+done
+
+# Prompt the user to enter a message
+echo "Enter a message:"
+read message
+
+# Prompt the user to select a recipient or add a new one
+echo "Select an option:"
+select option in "Select a recipient" "Add a new recipient"
+do
+  case $option in
+    "Select a recipient")
+      # Prompt the user to select a recipient from the phone book
+      echo "Select a recipient:"
+      select number in "${!phone_book[@]}"
+      do
+        if [[ -n $number ]]; then
+          recipient_name=${phone_book[$number]}
+          break
+        fi
+      done
+      break
+      ;;
+    "Add a new recipient")
+      # Call the add_number function to add a new number to the phone book
+      add_number
+      # Prompt the user to select the newly added recipient
+      echo "Select the newly added recipient:"
+      select number in "${!phone_book[@]}"
+      do
+        if [[ -n $number ]]; then
+          recipient_name=${phone_book[$number]}
+          break
+        fi
+      done
+      break
+      ;;
+  esac
+done
 
 # Prompt the user to select whether to schedule the message or send it immediately
 echo "Do you want to schedule the message? (y/n)"
@@ -34,30 +99,30 @@ if [[ $schedule == "y" ]]; then
   echo "Enter the schedule date and time in the format \"YYYY-MM-DD HH:MM:SS\" (e.g. 2023-02-24 10:00:00):"
   read schedule_time
   schedule_time=$(date -d "$schedule_time" '+%s')
-  request_body="{
+recipient_name="{
     \"messages\": [
         {
             \"source\": \"bash\",
-            \"body\": \"$message_body\",
-            \"to\": \"$to_number\",
+            \"body\": \"$message\",
+            \"to\": \"$number\",
             \"schedule\": $schedule_time
         }
     ]
 }"
 else
-  request_body="{
+recipient_name="{
     \"messages\": [
         {
             \"source\": \"bash\",
-            \"body\": \"$message_body\",
-            \"to\": \"$to_number\"
+            \"body\": \"$message\",
+            \"to\": \"${number//,}\"
         }
     ]
 }"
 fi
 
 # Send the API request
-response=$(curl --silent --show-error --request POST --url $url --header "${headers[0]}" --header "${headers[1]}" --data-binary "$request_body")
+response=$(curl --silent --show-error --request POST --url $url --header "${headers[0]}" --header "${headers[1]}" --data-binary "$recipient_name")
 
 # Print the API response
 echo "$response" | jq
